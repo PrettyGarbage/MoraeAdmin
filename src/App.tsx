7918f54ac0e5892546bcbc295fbde53459aff2cc
Pretty Garbage <hiko1931@gmail.com>
2022-10-03 20:57:14 +0900

    환경 세팅 다시 잡고 App.tsx 수정함

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Header from "./components/common/Header";
+import MainPage from "./components/MainPage";
+import EmptyPage from "./components/EmptyPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<MainPage/>} />
+          <Route path="*" element={<EmptyPage/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
